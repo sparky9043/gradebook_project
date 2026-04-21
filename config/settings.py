fd4974967f95a8cb2fd07d4a4635942d699dff17
Se Joon Park <sparky9043@gmail.com>
2026-04-21 17:24:27 +0900

    feat: add accounts urls and display html

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_URL = 'accounts:login_view'
