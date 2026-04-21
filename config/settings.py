58dc4bdbcd255bcf7351f3cfad2135602e720eb6
Se Joon Park <sparky9043@gmail.com>
2026-04-21 14:18:14 +0900

    build: add paths for static files and templates

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,4 +114,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
