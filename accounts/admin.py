b1878e0f9c32e29d53ede7c9fca73da19dd2ef7e
Se Joon Park <sparky9043@gmail.com>
2026-04-27 20:35:33 +0900

    feat: create and register Teacher to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .forms import TeacherCreationForm, TeacherChangeForm
+from .models import Teacher
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+class TeacherAdmin(UserAdmin):
+    model = Teacher
+    form = TeacherChangeForm
+    add_form = TeacherCreationForm
+    list_display = [
+        "username",
+        "email",
+        "department",
+        "is_staff",
+    ]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("department",),
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": ("department",),
+            },
+        ),
+    )
+
+
+admin.site.register(Teacher, TeacherAdmin)
