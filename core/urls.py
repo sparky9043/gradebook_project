664f28ed821e3c2e65aa68c34a913c7917a11039
Se Joon Park <sparky9043@gmail.com>
2026-04-27 21:16:20 +0900

    feat: remove ping/ path and add docstring to index view

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'core'
+app_name = "core"
 
 urlpatterns = [
-    # Core app main paths
-    path('', views.index, name="index"),
-
-    # ping path of con-jobs
-    path('ping/', views.ping, name="ping"),
+    path("", views.index, name="index"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    context = { 'show_nav': True, 'show_footer': True, 'show_sticky': True }
-    return render(request, 'core/index.html', context)
-
-def ping(request):
-    return render(request, 'core/ping.html')
+    """Display index at main url"""
+    context = {"show_nav": True, "show_footer": True, "show_sticky": True}
+    return render(request, "core/index.html", context)
