51ce54993a6285f2074df199cd0f1915389d548d
Se Joon Park <sparky9043@gmail.com>
2026-04-27 20:26:43 +0900

    feat: create Teacher model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class Teacher(AbstractUser):
+    is_staff = models.BooleanField(default=True)
+    department = models.CharField(
+        max_length=50,
+        choices=[
+            ("Math", "Math"),
+            ("Science", "Science"),
+            ("English", "English"),
+            ("History", "History"),
+        ],
+    )
