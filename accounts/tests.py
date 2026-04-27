ddb89b83d9c5b613dcd3e2ffc96b8fec8379aca2
Se Joon Park <sparky9043@gmail.com>
2026-04-27 20:49:07 +0900

    test: create superuser test

--- accounts/tests.py
+++ accounts/tests.py
@@ -16,3 +16,20 @@ class TeacherManagerTests(TestCase):
 
         self.assertEqual(new_user.username, "testuser")
         self.assertEqual(new_user.email, "testuser@example.com")
+        self.assertTrue(new_user.is_active)
+        self.assertFalse(new_user.is_staff)
+        self.assertFalse(new_user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        superuser = User.objects.create_superuser(
+            username="testuser",
+            password="testuser1234",
+            email="testuser@example.com",
+        )
+
+        self.assertEqual(superuser.username, "testuser")
+        self.assertEqual(superuser.email, "testuser@example.com")
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
