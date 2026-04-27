3fa88dc374993cd4753d854171998fa2a015414f
Se Joon Park <sparky9043@gmail.com>
2026-04-27 20:43:12 +0900

    feat: write test for creating teacher

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+
+class TeacherManagerTests(TestCase):
+    def test_create_teacher(self):
+        User = get_user_model()
+        new_user = User.objects.create_user(
+            username="testuser",
+            password="testuser1234",
+            email="testuser@example.com",
+        )
+
+        self.assertEqual(new_user.username, "testuser")
+        self.assertEqual(new_user.email, "testuser@example.com")
