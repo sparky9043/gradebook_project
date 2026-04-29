226f1ba9403973cf9b4af7d053bfeb92cb0bb17b
Se Joon Park <sparky9043@gmail.com>
2026-04-29 14:57:33 +0900

    feat: require login for gradebook home and add style to template

--- gradebook/views.py
+++ gradebook/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 
-class GradeLogMainView(TemplateView):
+class GradeLogMainView(LoginRequiredMixin, TemplateView):
     template_name = "gradebook/home.html"
+    login_url = reverse_lazy("accounts:login_view")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["show_nav"] = True
+        context["show_footer"] = True
+        return context
