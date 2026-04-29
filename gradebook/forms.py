5cc749aa2cce71758a184bf37375ac31e2418552
Se Joon Park <sparky9043@gmail.com>
2026-04-29 16:34:17 +0900

    feat: add styles to forms

--- gradebook/forms.py
+++ gradebook/forms.py
@@ -11,6 +11,16 @@ class CourseCreationForm(forms.ModelForm):
             "teacher",
         ]
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields["title"].widget.attrs.update({"class": "border-2 "})
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs.update(
+            {
+                "class": "w-full bg-surface-container-low border-outline-variant border-opacity-15 rounded-lg px-5 py-4 focus:ring-4 focus:ring-primary-fixed focus:border-primary transition-all duration-300 font-body text-lg outline-none placeholder:text-outline/40",
+                "placeholder": "e.g. The Ethics of Ancient Philosophy",
+            }
+        )
+        self.fields["teacher"].widget.attrs.update(
+            {
+                "class": "w-full appearance-none bg-surface-container-low border-outline-variant border-opacity-15 rounded-lg px-5 py-4 focus:ring-4 focus:ring-primary-fixed focus:border-primary transition-all duration-300 font-body text-lg outline-none cursor-pointer",
+            }
+        )
