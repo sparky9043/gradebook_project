064d1658c75f48e0c1d78c64a5cf604a25ed1bd3
Se Joon Park <sparky9043@gmail.com>
2026-05-04 19:36:18 +0900

    build: change to local db

--- config/settings.py
+++ config/settings.py
@@ -91,15 +91,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     # SUPABASE
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-    )
+    # "default": dj_database_url.config(
+    #     default=os.getenv("DATABASE_URL"),
+    #     conn_max_age=600,
+    # )
     # LOCAL DATABASE FOR TESTING
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
