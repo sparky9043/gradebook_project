d8f4de4fa400eb0665bb56eacb2b2baf06f8684e
Se Joon Park <sparky9043@gmail.com>
2026-05-04 19:32:21 +0900

    build: change to supabase and register Student and Enrollment to admin site

--- gradebook/admin.py
+++ gradebook/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Student, Enrollment
 
 # Register your models here.
 
 admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Enrollment)
