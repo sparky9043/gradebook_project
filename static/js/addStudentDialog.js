a56d71ef9200cf5a7fb0e68055ab897d2dc4c197
Se Joon Park <sparky9043@gmail.com>
2026-05-06 15:33:20 +0900

    feat: add event listeners to show and close dialog buttons

--- static/js/addStudentDialog.js
+++ static/js/addStudentDialog.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM loaded');
+  // Show Dialog Button
+  const showStudentDialogBtn = document.querySelector('#show-student-dialog-btn');
+
+  // Close Dialog Buttons
+  const closeStudentDialogBtn = document.querySelector('#close-student-dialog-btn');
+  const cancelAddStudentBtn = document.querySelector('#cancel-add-student-btn');
+
+  // Dialog Element
+  const addStudentDialog = document.querySelector('dialog#add-student-dialog');
+  
+
+  // Event Listener on Show Dialog Button
+  showStudentDialogBtn.addEventListener('click', () => {
+    addStudentDialog.showModal();
+  });
+  
+
+  // Event Listener on both Close Dialog Buttons
+  const cancelButtons = [closeStudentDialogBtn, cancelAddStudentBtn];
+
+  cancelButtons.forEach((button) => button.addEventListener('click', () => {
+    addStudentDialog.close();
+  }));
 });
