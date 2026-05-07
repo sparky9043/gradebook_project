1df792d9d085f3ba32c0571706b99decdd6a10b0
Se Joon Park <sparky9043@gmail.com>
2026-05-07 15:43:03 +0900

    refactor: move js functions into one file

---
+++ static/js/script.js
@@ -0,0 +1,44 @@
+const addStudentDialog = () => {
+document.addEventListener('DOMContentLoaded', () => {
+  // Show Dialog Button
+    const showStudentDialogBtn = document.querySelector('#show-student-dialog-btn');
+
+    // Close Dialog Buttons
+    const closeStudentDialogBtn = document.querySelector('#close-student-dialog-btn');
+    const cancelAddStudentBtn = document.querySelector('#cancel-add-student-btn');
+
+    // Dialog Element
+    const addStudentDialog = document.querySelector('dialog#add-student-dialog');
+    
+
+    // Event Listener on Show Dialog Button
+    if (addStudentDialog) {
+      showStudentDialogBtn.addEventListener('click', () => {
+        addStudentDialog.showModal();
+      });
+      // Event Listener on both Close Dialog Buttons
+      const cancelButtons = [closeStudentDialogBtn, cancelAddStudentBtn];
+    
+      cancelButtons.forEach((button) => button.addEventListener('click', () => {
+        addStudentDialog.close();
+      }));
+    }
+    
+
+  });  
+}
+
+
+const addToastMessage = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      const messageContainer = document.querySelector('#message-container');
+      while(messageContainer.firstChild) {
+        messageContainer.removeChild(messageContainer.firstChild);
+      }
+    }, 5000);
+  });  
+}
+
+addStudentDialog();
+addToastMessage();
--- static/js/toastMessage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    const messageContainer = document.querySelector('#message-container');
-    while(messageContainer.firstChild) {
-      messageContainer.removeChild(messageContainer.firstChild);
-    }
-  }, 5000);
-});
