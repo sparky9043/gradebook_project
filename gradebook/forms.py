ba881a1d5b462c6ed04148a49306bbef1c85a5e2
Se Joon Park <sparky9043@gmail.com>
2026-05-14 14:49:30 +0900

    feat: add styles to student edit page

--- gradebook/forms.py
+++ gradebook/forms.py
@@ -30,3 +30,32 @@ class StudentCreationForm(forms.ModelForm):
             "dob",
             "grade_level",
         ]
+        widgets = {
+            "dob": forms.DateInput(
+                attrs={"type": "date"},
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs.update(
+            {
+                "class": "h-12 px-sm border border-outline focus:border-2 focus:border-primary-container bg-surface-container-lowest font-body-md text-body-md outline-none transition-all"
+            }
+        )
+        self.fields["last_name"].widget.attrs.update(
+            {
+                "class": "h-12 px-sm border border-outline focus:border-2 focus:border-primary-container bg-surface-container-lowest font-body-md text-body-md outline-none transition-all"
+            }
+        )
+
+        self.fields["dob"].widget.attrs.update(
+            {
+                "class": "h-12 px-sm border border-outline focus:border-2 focus:border-primary-container bg-surface-container-lowest font-body-md text-body-md outline-none transition-all"
+            }
+        )
+        self.fields["grade_level"].widget.attrs.update(
+            {
+                "class": "h-12 px-sm border border-outline focus:border-2 focus:border-primary-container bg-surface-container-lowest font-body-md text-body-md outline-none transition-all appearance-none cursor-pointer"
+            }
+        )
